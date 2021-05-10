e558db6f38d46781d8050c5e4b03551a5ec2f89b
Jean Manzo <jdevmanzo@gmail.com>
2021-05-10 15:51:37 -0500

    initial commit

---
+++ cemeraparts-netsuite-integration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package CemerapartsNetsuiteIntegration
+ */
+/**
+ * Plugin Name: Cemeraparts Netsuite Integration
+ * Plugin URI: https://cemeraparts.com
+ * Description: This is a custom plugin built for Cemeraparts company. It's a custom netsuite integration.
+ * Version: 1.0.0
+ * Author: Britecode
+ * Author URI: https://britecode.io
+ * License: GPLv2 or later
+ * Text Domain: cemeraparts_netsuite_integration
+ */
