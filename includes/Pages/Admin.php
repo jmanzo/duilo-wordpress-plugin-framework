ec10a8f976425ef85a7e69a7336170ca8aa24a41
Jean Manzo <jdevmanzo@gmail.com>
2021-05-20 11:19:32 -0500

    starting to add submenus + defined the two subpages that will be present

--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -14,6 +14,8 @@ class Admin extends Controller
 
     public $pages = array();
 
+    public $subpages = array();
+
     public function __construct()
     {
         $this->settings = new Settings();
@@ -31,10 +33,25 @@ class Admin extends Controller
                 'position' => 110
             )
         );
+
+        $this->subpages = array(
+			array(
+				'parent_slug' => 'duilo_netsuite_integration', 
+				'page_title' => 'Settings', 
+				'menu_title' => 'Settings', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'duilo_netsuite_integration_settings', 
+				'callback' => function() { echo '<h1>Settings</h1>'; }
+			)
+		);
     }
 
     public function register()
     {
-        $this->settings->addPages( $this->pages )->register();
+        $this->settings
+            ->addPages( $this->pages )
+            ->withSubPage( 'Integration' )
+            ->addSubPages( $this->subpages )
+            ->register();
     }
 }
