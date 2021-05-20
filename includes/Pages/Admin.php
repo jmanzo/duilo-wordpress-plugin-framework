cc4df1de436c7fc50b5d7eff538aa64b4a4c3519
Jean Manzo <jdevmanzo@gmail.com>
2021-05-20 10:38:32 -0500

    modularized the ApiSettings class so plugin can create new elements recursively

--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -6,21 +6,35 @@
 namespace Inc\Pages;
 
 use \Inc\Controller;
+use \Inc\Api\Settings;
 
 class Admin extends Controller
 {
+    public $settings;
+
+    public $pages = array();
+
+    public function __construct()
+    {
+        $this->settings = new Settings();
+
+        $this->pages = array(
+            array(
+                'page_title' => 'Duilo Netsuite Integration', 
+                'menu_title' => 'Duilo NS', 
+                'capability' => 'manage_options', 
+                'menu_slug' => 'duilo_netsuite_integration', 
+                'callback' => function() {
+                    echo '<h1>Duilo Netsuite Integration</h1>';
+                }, 
+                'icon_url' => 'dashicons-store', 
+                'position' => 110
+            )
+        );
+    }
+
     public function register()
     {
-        add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-    }
-
-    public function add_admin_pages()
-    {
-        add_menu_page( 'Duilo Netsuite Integration', 'Duilo NS', 'manage_options', 'duilo_netsuite_integration', array( $this, 'admin_index' ), 'dashicons-store', 110 );
-    }
-
-    public function admin_index()
-    {
-        require_once $this->plugin_path . 'templates/admin.php';
+        $this->settings->addPages( $this->pages )->register();
     }
 }
