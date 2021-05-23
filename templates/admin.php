da7b321f91e3560da9cf4de7e5e50c8f61572ade
Jean Manzo <jdevmanzo@gmail.com>
2021-05-22 22:17:02 -0500

    added tab-panel system to the admin menu page to test with this kind of integration

--- templates/admin.php
+++ templates/admin.php
@@ -2,10 +2,37 @@
 
 <?php settings_errors() ?>
 
-<form action="options.php" method="post">
-    <?php
-        settings_fields( 'duilo_netsuite_options_group' );
-        do_settings_sections( 'duilo_netsuite_plugin' );
-        submit_button();
-    ?>
-</form>
+<ul class="nav nav-tabs">
+    <li class="tab active">
+        <a href="#tab-1">General</a>
+    </li>
+
+    <li class="tab">
+        <a href="#tab-2">Settings</a>
+    </li>
+
+    <li class="tab">
+        <a href="#tab-3">About</a>
+    </li>
+</ul>
+
+<div class="tab-content">
+    <div class="tab-pane active" id="tab-1">
+        <h3>General</h3>
+        <form action="options.php" method="post">
+            <?php
+                settings_fields( 'duilo_netsuite_options_group' );
+                do_settings_sections( 'duilo_netsuite_plugin' );
+                submit_button();
+            ?>
+        </form>
+    </div>
+
+    <div class="tab-pane" id="tab-2">
+        <h3>Settings</h3>
+    </div>
+
+    <div class="tab-pane" id="tab-3">
+        <h3>About</h3>
+    </div>
+</div>
